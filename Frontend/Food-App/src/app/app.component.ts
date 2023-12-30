62c5fea05b6bb267b607ce0ba6f3ba484b9ce880
alexeod <90037308+apravchev@users.noreply.github.com>
2023-12-30 23:35:24 +0200

    Added profile info tab, stylized nav bar and logo

--- Frontend/Food-App/src/app/app.component.ts
+++ Frontend/Food-App/src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, NavBarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    NavBarComponent,
+    ProfileInfoComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
